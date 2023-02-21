10f5b4f7942eb0a3b99368534977d8bda77efad8
David <106770826+davidSalamancaGonzalez@users.noreply.github.com>
2023-02-21 18:41:17 +0100

    input down data

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home/home.component';
-import { InputDataComponent } from './shared/input-data/input-data.component';
+
 
 const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'datatable', component: DatatableComponent },
-  { path: 'home', component: InputDataComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'home'}
 ];
 
