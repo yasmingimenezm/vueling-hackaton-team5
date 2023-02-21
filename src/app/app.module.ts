b07e7a99e81fde0055b49f97d7e05888bb8724ba
0410L <oriolbusquet@gmail.com>
2023-02-21 15:17:16 +0100

    login2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
+
 import { LoginComponent } from './login/login.component';
 import { SpinnerComponent } from './shared/spinner/spinner.component';
-=======
+
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { DatatableComponent } from './datatable/datatable.component';
->>>>>>> 863731999ab4096377ad1abfc042c4ca3b25bec9
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
+
     LoginComponent,
-SpinnerComponent
-=======
+    SpinnerComponent,
     HeaderComponent,
     FooterComponent,
     DatatableComponent
->>>>>>> 863731999ab4096377ad1abfc042c4ca3b25bec9
+
   ],
   imports: [
     BrowserModule,
