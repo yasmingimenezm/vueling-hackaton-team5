d5a9c9a640a399c10bcca33ddf623f9fd366d720
yasmin.gimenez <yasmin.gimenez@col.vueling.com>
2023-02-21 13:10:37 +0100

    Added header and footer

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  public userLogged: boolean = false;
+
+  userLogin() {
+    this.userLogged = true;
+  }
+
+  userLogout() {
+    this.userLogged = false;
+  }
+
 }
