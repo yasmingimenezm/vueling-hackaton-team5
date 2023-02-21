82411aa408b4d8d223a478f4cfc6ece45f8fed6b
0410L <oriolbusquet@gmail.com>
2023-02-21 17:44:41 +0100

    csv

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { HomeComponent } from './home/home/home.component';
 import { ErrorComponent } from './shared/error/error.component';
+import { Json2csvComponent } from './shared/json2csv/json2csv.component';
 
 
 
@@ -27,6 +28,7 @@ import { ErrorComponent } from './shared/error/error.component';
     DatatableComponent,
     HomeComponent,
     ErrorComponent,
+    Json2csvComponent,
 
 
   ],
