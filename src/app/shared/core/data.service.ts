4d9d6d129af3ea690afc19f4e9920a15c849eb50
David <106770826+davidSalamancaGonzalez@users.noreply.github.com>
2023-02-21 18:48:33 +0100

    p

--- src/app/shared/core/data.service.ts
+++ src/app/shared/core/data.service.ts
@@ -2,6 +2,7 @@ import { environment } from './../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { take } from 'rxjs';
 import { HttpClient } from "@angular/common/http";
+import { IUser } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,20 +17,20 @@ export class DataService {
     return this.http.post('url ', data)
   }
 
-  userAuth1(user: string, password: string): IUser/* Observable<IUser> */ {
-    if (user === 'admin') {
-      return {
-        user: 'admin',
-        role: 'admin',
-        token: '1234567890'
-      }
-    } else {
-      return {
-        user: 'userexample',
-        role: 'user',
-        token: '1234567890'
-      }
-    }
+  // userAuth1(user: string, password: string): IUser/* Observable<IUser> */ {
+  //   if (user === 'admin') {
+  //     return {
+  //       user: 'admin',
+  //       role: 'admin',
+  //       token: '1234567890'
+  //     }
+  //   } else {
+  //     return {
+  //       user: 'userexample',
+  //       role: 'user',
+  //       token: '1234567890'
+  //     }
+  //   }
 
   userAuth(user: string): any {
     return this.http.get(`${environment.apiUrl}userAuth/${user}`).pipe(take(1));
